c720bde9c420c85d9745f04e1e10b6be4ae29d42
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-17 19:18:51 +0100

    generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_charts do
+  name 'Redmine Charts plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
