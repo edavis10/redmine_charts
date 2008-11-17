d9ee59560f17167b226f765117c0c0bf7c8fa21d
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-17 19:26:29 +0100

    groups chart controller

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_charts do
-  name 'Redmine Charts plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+Redmine::Plugin.register :charts do
+  name 'Charts'
+  author 'Maciej Szczytowski'
+  description 'Charts plugin contains some useful project statistics.'
   version '0.0.1'
+  
+  menu :project_menu, :charts, { :controller => 'charts/groups', :action => 'index' }, :caption => ':charts_menu_label', :after => :activity, :param => :project_id
 end
