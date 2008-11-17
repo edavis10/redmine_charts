8bf6f4d6f43b5501b209f9f04863dcf0d787a58e
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-17 19:58:38 +0100

    add project menu

--- init.rb
+++ init.rb
@@ -5,6 +5,8 @@ Redmine::Plugin.register :charts do
   author 'Maciej Szczytowski'
   description 'Charts plugin contains some useful project statistics.'
   version '0.0.1'
-  
-  menu :project_menu, :charts, { :controller => 'charts/groups', :action => 'index' }, :caption => ':charts_menu_label', :after => :activity, :param => :project_id
+
+  permission :charts, {"charts/groups".to_sym => [:index], "charts/hours".to_sym => [:index], "charts/velocity".to_sym => [:index]}, :public => true
+    
+  menu :project_menu, :charts, { :controller => 'charts/groups', :action => 'index' }, :caption => :charts_menu_label, :after => :new_issue, :param => :project_id
 end
