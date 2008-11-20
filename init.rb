f12ab3d367728947e2ee9b9200e54e42dfca5705
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-20 21:12:11 +0100

    making burndown as default chart

--- init.rb
+++ init.rb
@@ -11,5 +11,5 @@ Redmine::Plugin.register :charts do
 
   permission :charts, {"charts_groups".to_sym => [:index], "charts_hours".to_sym => [:index], "charts_burndown".to_sym => [:index]}, :public => true
     
-  menu :project_menu, :charts, { :controller => 'charts_groups', :action => 'index' }, :caption => :charts_menu_label, :after => :new_issue, :param => :project_id
+  menu :project_menu, :charts, { :controller => 'charts_burndown', :action => 'index' }, :caption => :charts_menu_label, :after => :new_issue, :param => :project_id
 end
