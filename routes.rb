2abbb6b8d8df318e31d557d05b2a635437fbbc61
Maciej Szczytowski <mszczytowski@gmail.com>
2008-11-20 17:33:23 +0100

    add burndown chart route

--- routes.rb
+++ routes.rb
@@ -1,3 +1,3 @@
 connect 'projects/:project_id/charts/groups/:action', :controller => 'charts_groups'
-connect 'projects/:project_id/charts/velocity/:action', :controller => 'charts_velocity'
+connect 'projects/:project_id/charts/burndown/:action', :controller => 'charts_burndown'
 connect 'projects/:project_id/charts/hours/:action', :controller => 'charts_hours'
