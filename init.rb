ef52f1b1d7a55ee52b9dba4e55cf078df4453048
Maciej Szczytowski <mszczytowski@gmail.com>
2009-01-26 18:10:20 +0100

    next version

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@ Redmine::Plugin.register :charts_plugin do
   author 'Maciej Szczytowski'
   description 'Plugin for Redmine to show Your projects\' charts.'
   url 'http://github.com/mszczytowski/redmine_charts/'
-  version '0.0.9'
+  version '0.0.10'
 
   # Minimum version of Redmine.
 
